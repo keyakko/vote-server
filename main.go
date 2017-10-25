2f8a76d3add765bf8c4df79d54e71468b6051617
keyakko <keyakko.web@gmail.com>
2017-10-25 13:12:56 +0900

    リクエスト送信用スクリプトの編集。 送信済みのスクリプト数を画面へ表示するよう変更

--- main.go
+++ main.go
@@ -4,22 +4,21 @@ import "fmt"
 import "net/url"
 import "net/http"
 
-func post_req() {
-	//いい感じに投げて？
-//	//ちゃんとPOSTで投げるんやで
-//	v := url.Values{}
-//	v.Set(param_key, param_val)
+func post_req(total *int) {
 	http.PostForm("http://localhost/voting.php", url.Values{"vote": {"fenecc"}})
+	*total++
+	//fmt.Println(*total)
 }
 
 func main() {
-//	dist_url := "http://172.16.10.1/voting.php"
-//	param_key := "vote"
-//	param_val := "fenecc"
+	total := 0
 	end := 1000000
 	for i := 0; i < end; i++ {
-		go post_req()
-		stat_code := "hoge"
-		fmt.Println(stat_code)
+		go post_req(&total)
+		fmt.Println(total)
+	}
+	for ;total != end; {
+		fmt.Println(total)
+		continue;
 	}
 }
