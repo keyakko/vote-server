b78857db7c0e1d4d077f2755ead45648fb9e9fb2
keyakko <keyakko.web@gmail.com>
2017-11-02 13:20:24 +0900

    送信するリクエスト量を少なく、再送するよう変更

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 import "net/url"
 import "net/http"
+import "time"
 
 func post_req(total *int) {
 	_, err := http.PostForm("http://127.0.0.1/voting.php", url.Values{"vote": {"1"}})
 	if err != nil {
 		fmt.Println(err)
+		post_req(total)
 	} else {
 		*total++
 	}
@@ -15,10 +17,11 @@ func post_req(total *int) {
 
 func main() {
 	total := 0
-	end := 1000000
+	end := 50000
 	for i := 0; i < end; i++ {
 		go post_req(&total)
 		fmt.Println(total)
+		time.Sleep(100)
 	}
 	for ;total != end; {
 		fmt.Println(total)
