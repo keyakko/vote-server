16a6e4188cfe30b4bfec0fd7816e12c785e6f260
keyakko <keyakko.web@gmail.com>
2017-11-02 13:38:34 +0900

    リクエストが確実に送信されるよう変更

--- main.go
+++ main.go
@@ -3,28 +3,27 @@ package main
 import "fmt"
 import "net/url"
 import "net/http"
-import "time"
+import "sync"
 
-func post_req(total *int) {
-	_, err := http.PostForm("http://127.0.0.1/voting.php", url.Values{"vote": {"1"}})
+func post_req(total *int, wg *sync.WaitGroup) {
+	_, err := http.PostForm("http://127.0.0.1/voting.php", url.Values{"vote": {"3"}})
 	if err != nil {
 		fmt.Println(err)
-		post_req(total)
+		post_req(total, wg)
 	} else {
 		*total++
+		wg.Done()
 	}
 }
 
 func main() {
+	var wg sync.WaitGroup
 	total := 0
 	end := 50000
 	for i := 0; i < end; i++ {
-		go post_req(&total)
+		wg.Add(1)
+		go post_req(&total, &wg)
+		wg.Wait()
 		fmt.Println(total)
-		time.Sleep(100)
-	}
-	for ;total != end; {
-		fmt.Println(total)
-		continue;
 	}
 }
